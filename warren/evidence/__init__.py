4efcb3892c971df4fb3046f086e78922feaedc48
drmaganti <ram.maganti@outlook.com>
2026-09-01 21:59:41 -0400

    Export Warren evidence router

--- warren/evidence/__init__.py
+++ warren/evidence/__init__.py
@@ -1,11 +1,14 @@
 from .composite import CompositeEvidenceProvider
 from .fred import FredMacroEvidenceProvider
+from .router import EvidenceRouter, normalize_claims
 from .sec import SecFilingEvidenceProvider
 from .yahoo import YahooEvidenceProvider
 
 __all__ = [
     "CompositeEvidenceProvider",
+    "EvidenceRouter",
     "FredMacroEvidenceProvider",
     "SecFilingEvidenceProvider",
     "YahooEvidenceProvider",
+    "normalize_claims",
 ]
